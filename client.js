26e07767e0ef17fa411a5e15e848a7734730758b
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2025-06-03 04:36:42 +0800

    feat: add supabase-client

--- client.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import 'dotenv/config'
-const supabase_key = process.env.SUPABASE_KEY;
-const supabase = createClient("https://muenrhsbneislbcuqcuq.supabase.co", supabase_key);

670b9075217f63f02ca34b88cc5b0ca2560dc565
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2025-06-02 23:40:33 +0800

    feat: hello world server

---
+++ client.js
@@ -0,0 +1,5 @@
+
+import { createClient } from '@supabase/supabase-js'
+import 'dotenv/config'
+const supabase_key = process.env.SUPABASE_KEY;
+const supabase = createClient("https://muenrhsbneislbcuqcuq.supabase.co", supabase_key);

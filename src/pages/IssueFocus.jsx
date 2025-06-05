9166642dd4a596a5771da9b869d4d93270db3c60
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2025-06-05 10:47:29 +0800

    feat: add comment section, it probably works

--- src/pages/IssueFocus.jsx
+++ src/pages/IssueFocus.jsx
@@ -5,6 +5,25 @@ import { Link, useNavigate, useParams } from "react-router";
 import ReCAPTCHA from "react-google-recaptcha";
 const recaptchaRef = createRef();
 
+function Comment({ comment }) {
+    if (comment?.users?.username) {
+        return (
+            <div>
+                {comment.comment_text}
+                {comment.users.username}
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                {comment.comment_text}
+                Anonymous User
+            </div>
+        );
+    }
+    return;
+}
+
 function UserProfile({ token }) {
     const { issue_id } = useParams();
     const navigate = useNavigate();
@@ -14,6 +33,8 @@ function UserProfile({ token }) {
         // apply to form data
     };
     const [data, setData] = useState();
+    const [comment_data, setCommentData] = useState();
+    const [comment, setComment] = useState();
     const table = [];
     async function getIssues() {
         const { data: user_data, error: user_error } = await supabase.auth.getUser();
@@ -31,9 +52,49 @@ function UserProfile({ token }) {
             return data;
         }
     }
+    async function getComments() {
+        const { data: user_data, error: user_error } = await supabase.auth.getUser();
+        if (user_error) {
+            console.log(user_error);
+            return "";
+        }
+        const { data, error } = await supabase
+            .from("comments")
+            .select("*, users(username)")
+            .eq("issue_id", parseInt(issue_id));
+        if (error) {
+            console.log(error);
+        } else {
+            return data;
+        }
+    }
+    async function formSubmit(e) {
+        e.preventDefault();
+        const { data, error: user_error } = await supabase.auth.getUser();
+        if (user_error) {
+            // console.log(user_error);
+            return;
+        }
+        const { error } = await supabase
+            .from("comments")
+            .insert({
+                issue_id: parseInt(issue_id),
+                comment_text: comment,
+                user_id: data.user.id,
+            })
+            .single();
+        if (error) {
+            alert("Error posting comment, please try again later.");
+            console.log(error);
+        } else {
+            alert("Comment Sent");
+            window.location.reload();
+        }
+    }
     useEffect(() => {
         if (!data) getIssues().then(setData); // enforce run once
-    }, [data]);
+        if (!comment_data) getComments().then(setCommentData); // enforce run once
+    }, [data, comment_data]);
     if (data) {
         return (
             <div>
@@ -46,6 +107,24 @@ function UserProfile({ token }) {
                 {data.issue_state}
                 <br />
                 {data?.users?.username ? data.users.username : "Anonymous User"}
+                <br />
+                {comment_data.map((item, index) => {
+                    <Comment comment={item} />;
+                })}
+                <br />
+                <form onSubmit={formSubmit}>
+                    Comment:
+                    <br />
+                    <textarea
+                        name="Comment"
+                        type={"text"}
+                        value={comment}
+                        required
+                        placeholder="Comment"
+                        onChange={(event) => setComment(event.target.value)}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     } else {
